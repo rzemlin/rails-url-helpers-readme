7286fbac18b4b05bf8dbfbe3f5e844a13512c9c0
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-01-16 09:04:20 -0800

    commit tree bbe5b799225a54dc7387ce7fac49f85afb573349

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+Post.create(title: 'A lovely title', description: 'A superb description')
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
